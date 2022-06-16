674d19502b6f0d38725c4d08596edc76efa2cf47
felipemaxplay <felipemaxplay@gmail.com>
2022-06-16 15:42:06 -0300

    variables

--- hello.go
+++ hello.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello, world")
+	fmt.Printf("Hello, world\n")
+
+	a := 25
+	b := "world"
+	c := 1.921
+	d := false
+
+	fmt.Printf("%v -> %T\n", a, a)
+	fmt.Printf("%v -> %T\n", b, b)
+	fmt.Printf("%v -> %T\n", c, c)
+	fmt.Printf("%v -> %T\n", d, d)
 }
